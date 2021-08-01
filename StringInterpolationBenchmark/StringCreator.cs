38da76d0410b7782d35792e5be115fb1fe5e3a14
DenMich <miche@DESKTOP-8SMTMO8>
2021-08-01 17:56:29 +0200

    change approch with span

--- StringInterpolationBenchmark/StringCreator.cs
+++ StringInterpolationBenchmark/StringCreator.cs
@@ -7,7 +7,7 @@ namespace StringInterpolationBenchmark
     [MemoryDiagnoser]
     public class StringCreator
     {
-        [Params(100)]
+        [Params(100, 10_000)]
         public int charNumber { get; set; }
 
         private readonly Random _random = new Random();
@@ -47,6 +47,43 @@ namespace StringInterpolationBenchmark
         }
 
         [Benchmark]
+        public string GenerateStringSpanFromRandom()
+        {
+            unsafe
+            {
+                Span<char> result = stackalloc char[charNumber];
+
+                char offset = 'a';
+                int lettersOffset = 26;
+
+                for (var i = 0; i < charNumber; i++)
+                {
+                    var retChar = ((char)_random.Next(offset, offset + lettersOffset)).ToString().AsSpan();
+                    retChar.CopyTo(result.Slice(i, 1));
+                }
+
+                return result.ToString();
+            }
+        }
+
+        [Benchmark]
+        public string GenerateStringSpan()
+        {
+            unsafe
+            {
+                Span<char> result = stackalloc char[charNumber];
+                ReadOnlySpan<char> chars = "abcdefghijklmnopqrstuvwxyz";
+
+                for (var i = 0; i < charNumber; i++)
+                {
+                    chars.Slice(_random.Next(26), 1).CopyTo(result.Slice(i, 1));
+                }
+
+                return result.ToString();
+            }
+        }
+
+        [Benchmark]
         public string GenerateStringPlus()
         {
             string result = string.Empty;
@@ -61,38 +98,10 @@ namespace StringInterpolationBenchmark
         }
 
         [Benchmark]
-        public string GenerateStringPlusSpan()
-        {
-            string result = string.Empty;
-            ReadOnlySpan<char> chars = "abcdefghijklmnopqrstuvwxyz";
-
-            for (var i = 0; i < charNumber; i++)
-            {
-                result += chars[_random.Next(26)];
-            }
-
-            return result;
-        }
-
-        [Benchmark]
         public string GenerateStringBuilder()
         {
             var result = new StringBuilder();
             string chars = "abcdefghijklmnopqrstuvwxyz";
-
-            for (var i = 0; i < charNumber; i++)
-            {
-                result.Append(chars[_random.Next(26)]);
-            }
-
-            return result.ToString();
-        }
-
-        [Benchmark]
-        public string GenerateStringBuilderSpan()
-        {
-            var result = new StringBuilder();
-            ReadOnlySpan<char> chars = "abcdefghijklmnopqrstuvwxyz";
 
             for (var i = 0; i < charNumber; i++)
             {
